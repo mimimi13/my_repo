a9e43caf5238c4416b376bfac23a9ecc402f886e
mimi <mimi@myredbox.ru>
2017-05-18 21:47:22 +0300

    Поправил задание

--- lesson1/sqrt.rb
+++ lesson1/sqrt.rb
@@ -3,11 +3,10 @@ a = gets.chomp.to_f
 b = gets.chomp.to_f
 c = gets.chomp.to_f
 d = b ** 2 - 4 * a * c
-puts d
-d_sqrt = Math.sqrt(d)
 if d < 0
   puts "Корней нет"
 elsif d > 0
+  d_sqrt = Math.sqrt(d)
   x1 = ((-b) + d_sqrt) / (2 * a)
   x2 = ((-b) - d_sqrt) / (2 * a)
   puts "d = #{d}; Х1 = #{x1}; Х2 = #{x2}"
--- lesson1/triangle2.rb
+++ lesson1/triangle2.rb
@@ -4,5 +4,9 @@ b = gets.chomp.to_f
 c = gets.chomp.to_f
 sorted_values = [a,b,c].sort!
 
-puts "Треугольник равносторонний" if (sorted_values[0] == sorted_values[1]) && (sorted_values[0] ==sorted_values[2])
-puts "Треугольник равнобедренный" if (sorted_values[2] ** 2) == (sorted_values[0] ** 2 + sorted_values[1] ** 2)
+if (sorted_values[0] == sorted_values[1]) && (sorted_values[0] ==sorted_values[2])
+  puts "Треугольник равносторонний" 
+elsif (sorted_values[0] == sorted_values[1] || sorted_values[2] == sorted_values[0] || sorted_values[2] == sorted_values[1])
+  puts "Треугольник равнобедренный" 
+end
+puts "Треугольник прямоугольный" if (sorted_values[2] ** 2) == (sorted_values[0] ** 2 + sorted_values[1] ** 2)
